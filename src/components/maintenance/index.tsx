c232559ea174926b20ddcc1ac7766d917446b892
João Félix <felixxdeveloper@gmail.com>
2025-06-07 14:56:36 -0300

    REF - Creating maintenance page for default

---
+++ src/components/maintenance/index.tsx
@@ -0,0 +1,14 @@
+import * as s from './style';
+
+
+export default function MaintenancePage() {
+
+  return (
+    <s.Wrapper>
+      <s.Title>Voltaremos em breve!</s.Title>
+      <s.Subtitle>
+        Estamos realizando uma manutenção programada. Voltaremos em breve. Agradecemos a sua paciência.
+      </s.Subtitle>
+    </s.Wrapper>
+  )
+}
---
+++ src/components/maintenance/style.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  background: #f6f8fa;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  text-align: center;
+  flex-direction: column;
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  margin-bottom: 1rem;
+  color: #333;
+`;
+
+export const Subtitle = styled.p`
+  font-size: 1.2rem;
+  color: #666;
+  max-width: 600px;
+`;
