f0f0da30fc7755ed4f5bb9bb4f312249ccca024c
João Félix <felixxdeveloper@gmail.com>
2025-06-11 20:21:36 -0300

    REF - Fix layout bug on words (maintenance page)

--- src/components/maintenance/style.ts
+++ src/components/maintenance/style.ts
@@ -4,23 +4,34 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
-  background: #f6f8fa;
+
   display: flex;
   align-items: center;
-  justify-content: center;
-  padding: 2rem;
-  text-align: center;
   flex-direction: column;
+  justify-content: center;
+  gap: 1em;
+  
+  padding: 2rem;
+  
+  background-color: #f6f8fa;
+
+  text-align: center;
+
+
 `;
 
 export const Title = styled.h1`
   font-size: 2.5rem;
-  margin-bottom: 1rem;
+    
+  line-height: normal;
+
   color: #333;
 `;
 
 export const Subtitle = styled.p`
-  font-size: 1.2rem;
   color: #666;
+  font-size: 1.2rem;
+  line-height: normal;
+
   max-width: 600px;
 `;
